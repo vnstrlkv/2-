d72b85602cb0335c23340b9b1d305cbc0a74f7c3
IvanStrelkov <vnstrlkv@gmail.com>
2017-09-11 13:57:40 +0700

    upd

---
+++ srvr/srvr/Request.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using SQLite;
+
+
+namespace srvr
+{
+    class Request
+    {
+        [PrimaryKey, AutoIncrement, Unique]
+        public int Id { get; set; }
+
+        [MaxLength(30), NotNull]
+        public string FirstName { get; set; }
+
+        [MaxLength(30), NotNull]
+        public string LastName { get; set; }
+
+        [NotNull]
+        public DateTime BirthDate { get; set; }
+
+        [Ignore]
+        public string FullName
+        {
+            get
+            {
+                return string.Format(
+                    "{0} {1}",
+                    LastName,
+                    FirstName
+                );
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format(
+                "{0}: {1} {2}",
+                Id,
+                FullName,
+                BirthDate.ToString("dd-MM-yyyy")
+            );
+        }
+    }
+}
